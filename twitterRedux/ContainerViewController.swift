caae6f544c25f46d0d57460f08ecd841e57f7f77
Peter Bohnert <pbohnert@gmail.com>
2014-10-20 09:11:18 -0700

    Initial set of three controllers in Storyboard

--- twitterRedux/ContainerViewController.swift
+++ twitterRedux/ContainerViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ContainerViewController.swift
 //  twitterRedux
 //
 //  Created by Peter Bohnert on 10/20/14.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ContainerViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ twitterRedux/SidePanelViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SidePanelViewController.swift
+//  twitterRedux
+//
+//  Created by Peter Bohnert on 10/20/14.
+//  Copyright (c) 2014 Blue Lotus Labs. All rights reserved.
+//
+
+import UIKit
+
+class SidePanelViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
