1c264c7eac0af677f4b96919bf2c3773a24efbf7
Peter Bohnert <pbohnert@gmail.com>
2014-10-31 06:27:22 -0700

    updates for iOS 8.1

--- twitterRedux/TwitterClient.swift
+++ twitterRedux/TwitterClient.swift
@@ -38,7 +38,7 @@ class TwitterClient: BDBOAuth1RequestOperationManager {
         TwitterClient.sharedInstance.fetchRequestTokenWithPath("oauth/request_token", method: "GET", callbackURL: NSURL(string: "cptwitterredux2://oauth"), scope: nil, success: { (requestToken:BDBOAuthToken!) -> Void in
             //println("Got the request token from loginWithCompletion")
             var authURL = NSURL(string: "https://api.twitter.com/oauth/authorize?oauth_token=\(requestToken.token)")
-            UIApplication.sharedApplication().openURL(authURL)
+            UIApplication.sharedApplication().openURL(authURL!)
             }) { (error: NSError!) -> Void in
                 println("Failed to get the request token")
                 self.loginCompletion?(user: nil, error: error)
